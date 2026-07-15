ff65301a053aa5527fb02cff374a18c7f60738aa
Ansh Takwal <anshtakwal03@gmail.com>
2026-07-15 19:16:46 +0530

    Time: 0 ms (100%), Space: 43 MB (40.97%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int add = 0;
+        for(char ch :s.toCharArray()){
+            if(ch == '(') open++;
+            else if(ch==')' && open>0) open--;
+            else add++;       
+        }
+        return add+open;
+    }
+}
