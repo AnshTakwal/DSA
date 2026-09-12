4db14ca01fd4342c84a9b4fb1fa2c2bd159885c1
Ansh Takwal <anshtakwal03@gmail.com>
2026-09-12 12:56:29 +0530

    Time: 1 ms (99.97%), Space: 43.7 MB (22.56%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+       int i =  a.length() - 1;
+       int j =  b.length() - 1; 
+       int carry = 0;
+       while(i>=0 || j >=0 || carry > 0 ){
+        int digit1 = 0;
+        int digit2 = 0;
+        if(i>=0){
+            digit1 = a.charAt(i) - '0';
+            i--;
+        }
+        if(j>=0){
+            digit2 = b.charAt(j) - '0';
+            j--;
+        }
+        int sum = digit1 + digit2 + carry;
+        sb.append(sum%2);
+        carry = sum/2;
+       }
+       return sb.reverse().toString();
+    }
+}
